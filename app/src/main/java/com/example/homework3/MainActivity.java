c0f645ce5240b4be784171cc5da4d383c2e28525
Kristine <kreaselee@gmail.com>
2021-03-05 23:28:29 -0800

    Cleaned up code

--- app/src/main/java/com/example/homework3/MainActivity.java
+++ app/src/main/java/com/example/homework3/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.homework3;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -14,6 +15,7 @@ import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.squareup.picasso.Picasso;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -62,6 +64,39 @@ public class MainActivity extends AppCompatActivity {
                             // third tab - locations
                             if (position == 0) {
                                 String url = json.getString("characters");
+
+
+                                    client.addHeader("Accept", "application/json");
+                                    // send a get request to the api url
+                                    client.get(api_url, new AsyncHttpResponseHandler() {
+                                        @Override
+                                        public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                                            Log.d("api response", new String(responseBody));
+
+                                            try {
+                                                JSONObject json = new JSONObject(new String(responseBody));
+                                                JSONObject info = json.getJSONObject("info");
+
+                                                int count = info.getInt("count");
+                                                Random random = new Random();
+                                                int id = random.nextInt(count)+1;
+                                                String new_url = api_url + "/" + id;
+
+
+                                            }
+                                            catch (JSONException e) {
+                                                e.printStackTrace();
+                                            }
+
+                                        }
+
+                                        @Override
+                                        public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                                            Log.e("api error", new String(responseBody));
+                                        }
+                                    });
+
+
                                 // Bundle bundle = new Bundle();
                                 bundle.putString("url", url);
                                 CharacterFragment characterFragment = new CharacterFragment();
@@ -119,82 +154,5 @@ public class MainActivity extends AppCompatActivity {
         fragmentTransaction.commit();
     }
 
-    /*
-    public void loadCharacterFragment(Fragment fragment, String url) {
-        client.addHeader("Accept", "application/json");
-        // send a get request to the api url
-        client.get(url, new AsyncHttpResponseHandler() {
-            @Override
-            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
-                Log.d("api response", new String(responseBody));
-
-                try {
-                    JSONObject json = new JSONObject(new String(responseBody));
-                    JSONObject info = json.getJSONObject("info");
-
-                    int count = info.getInt("count");
-                    Random random = new Random();
-                    int id = random.nextInt(count)+1;
-                    String new_url = url + "/" + id;
-
-                    // send a get request to the api url
-                    client.get(new_url, new AsyncHttpResponseHandler() {
-                        @Override
-                        public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
-                            Log.d("api response", new String(responseBody));
-
-                            try {
-                                JSONObject json = new JSONObject(new String(responseBody));
-
-                                Bundle bundle = new Bundle();
-                                bundle.putString("name", json.getString("name"));
-                                bundle.putString("image", json.getString("image"));
-                                bundle.putString("status", json.getString("status"));
-                                bundle.putString("species", json.getString("species"));
-                                bundle.putString("gender", json.getString("gender"));
-                                JSONObject origin = json.getJSONObject("origin");
-                                bundle.putString("origin", origin.getString("name"));
-                                JSONObject location = json.getJSONObject("location");
-                                bundle.putString("location", location.getString("name"));
-                                JSONArray episode = json.getJSONArray("episode");
-                                ArrayList<String> episodes = new ArrayList<>();
-                                for (int i = 0; i < episode.length(); i++) {
-                                    String ep = episode.getString(i).replace("https://rickandmortyapi.com/api/episode/", "");
-                                    episodes.add(ep);
-                                }
-                                bundle.putString("episodes", episodes.toString());
-
-                                fragment.setArguments(bundle);
-                                FragmentManager fragmentManager = getSupportFragmentManager();
-                                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-                                fragmentTransaction.replace(R.id.fragmentContainerView, fragment);
-                                fragmentTransaction.commit();
-                            }
-                            catch (JSONException e) {
-                                e.printStackTrace();
-                            }
-                        }
-
-                        @Override
-                        public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
-                            Log.e("api error", new String(responseBody));
-                        }
-                    });
-                }
-                catch (JSONException e) {
-                    e.printStackTrace();
-                }
-
-            }
-
-            @Override
-            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
-                Log.e("api error", new String(responseBody));
-            }
-        });
-
-    }
-
-     */
 
 }
